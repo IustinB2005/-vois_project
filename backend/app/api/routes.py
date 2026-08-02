e4059149ac271f613e1bdde4548d874d082415ef
sebastiangm15 <sebastiangilma9@gmail.com>
2026-08-03 01:22:13 +0300

    create the requirements

---
+++ backend/app/api/routes.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate
+
+
+router = APIRouter()
+
+
+@router.post("/register")
+def register(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=user.password
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "id": new_user.id,
+        "username": new_user.username,
+        "email": new_user.email
+    }
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
+from app.core.database import Base, engine
+from app.models import user
+from app.api.routes import router
+
+
+Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI()
 
+
+app.include_router(router)
+
+
 @app.get("/")
-def home():
-    return {"message": "Server works"}
+def root():
+    return {
+        "message": "API running"
+    }
