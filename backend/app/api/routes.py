db5c43e5e6f91ae551ad9f756f9722de7b9b5cd0
sebastiangm15 <sebastiangilma9@gmail.com>
2026-08-04 01:40:19 +0300

    add more securiy with jwt token

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,10 +1,14 @@
+from fastapi import HTTPException
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
+from app.core.security import hash_password
+from app.core.security import verify_password
+from app.core.security import create_access_token
 from app.core.database import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate
-
+from app.schemas.user import UserCreate, UserLogin
+from app.api.dependencies import get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -18,7 +22,7 @@ def register(
     new_user = User(
         username=user.username,
         email=user.email,
-        password=user.password
+        password=hash_password(user.password)
     )
 
     db.add(new_user)
@@ -29,4 +33,86 @@ def register(
         "id": new_user.id,
         "username": new_user.username,
         "email": new_user.email
+    }
+
+
+@router.get("/users")
+def get_users(
+    db: Session = Depends(get_db)
+):
+
+    users = db.query(User).all()
+
+    return users
+
+@router.delete("/users/{user_id}")
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    db.delete(user)
+    db.commit()
+
+    return {
+        "message": "User deleted"
+    }
+
+
+@router.post("/login")
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+
+    db_user = db.query(User).filter(
+        User.email == form_data.username
+    ).first()
+
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+
+    if not verify_password(
+        form_data.password,
+        db_user.password
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect password"
+        )
+
+
+    access_token = create_access_token(
+        data={
+            "sub": db_user.email
+        }
+    )
+
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+@router.get("/me")
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email
     }
