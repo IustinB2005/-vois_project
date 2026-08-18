4093035f817a212a1161637d1af50c6b9e12edda
Daria <dariamarian2005.dm@gmail.com>
2026-08-18 22:36:02 +0300

    Adaugat variabile de mediu in docker-compose pentru securitate si login

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel, EmailStr
-from typing import Literal
+from typing import Literal,Optional
 
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
     # Utilizatorul va fi fortat sa aleaga exact UNA din aceste 6 variante:
-    preferred_decade: Literal["1970-1980", "1980-1990", "1990-2000", "2000-2010", "2010-2020", "2020+"]
-    preferred_genre: Literal["Actiune", "Comedie", "Drama", "Horror", "Romantic", "SF"]
-
+    preferred_decade: Optional[Literal["1970-1980", "1980-1990", "1990-2000", "2000-2010", "2010-2020", "2020+"]] = None
+    preferred_genre: Optional[Literal["Actiune", "Comedie", "Drama", "Horror", "Romantic", "SF"]] = None
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
